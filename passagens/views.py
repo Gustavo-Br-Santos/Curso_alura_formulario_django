875c75bb472f895fe102337113b9a56032b2508f
Gustavo Santos <gustavo.br.sant@hotmail.com>
2020-10-26 00:23:26 -0300

    criação do formulario inicial de passagens

---
+++ passagens/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from passagens.forms import PassagemForms
+
+def index(request):
+    form = PassagemForms()
+    context = {
+        'form': form
+    }
+    return render(request, 'index.html', context)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        context = {
+            'form': form
+        }
+        return render(request, 'minha_consulta.html', context)
